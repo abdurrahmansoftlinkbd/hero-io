28c1ea8b87328bda3d547bae4ca92abc66579424
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2026-03-18 08:48:39 +0600

    added MainLayout.jsx

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const MainLayout = () => {
+  return (
+    <div>
+      hello
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default MainLayout;
